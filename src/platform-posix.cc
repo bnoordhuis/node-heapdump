8c54b6fcf8f0571c2cf339b28032c3aee8988644
Ben Noordhuis <info@bnoordhuis.nl>
2013-07-17 23:38:42 +0200

    unix: reap child process
    Don't leave zombie processes behind, reap the child process when it's
    done writing out the snapshot.
    
    Fixes #16.

--- src/platform-posix.cc
+++ src/platform-posix.cc
@@ -24,21 +24,41 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 namespace heapdump
 {
 
 uv_signal_t signal_handle;
+uv_signal_t sigchld_handle;
+pid_t child_pid = -1;
 
-void OnSignal(uv_signal_t* handle, int signo)
+void OnSIGUSR2(uv_signal_t* handle, int signo)
 {
   assert(handle == &signal_handle);
   heapdump::WriteSnapshot();
 }
 
+void OnSIGCHLD(uv_signal_t* handle, int signo)
+{
+  assert(handle == &sigchld_handle);
+  int status;
+  pid_t pid = waitpid(child_pid, &status, WNOHANG);
+  assert(pid != -1);
+  if (pid == 0) return;
+  assert(pid == child_pid);
+  uv_signal_stop(&sigchld_handle);
+}
+
 void WriteSnapshot()
 {
-  if (fork() != 0) return;
+  if (uv_is_active(reinterpret_cast<uv_handle_t*>(&sigchld_handle))) return;
+  child_pid = fork();
+  if (child_pid == -1) return;
+  if (child_pid != 0) {
+    uv_signal_start(&sigchld_handle, OnSIGCHLD, SIGCHLD);
+    return;
+  }
   setsid();
   WriteSnapshotHelper();
   _exit(42);
@@ -46,8 +66,9 @@ void WriteSnapshot()
 
 void PlatformInit()
 {
+  uv_signal_init(uv_default_loop(), &sigchld_handle);
   uv_signal_init(uv_default_loop(), &signal_handle);
-  uv_signal_start(&signal_handle, OnSignal, SIGUSR2);
+  uv_signal_start(&signal_handle, OnSIGUSR2, SIGUSR2);
   uv_unref(reinterpret_cast<uv_handle_t*>(&signal_handle));
 }
 
