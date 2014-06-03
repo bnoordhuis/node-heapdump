d8c0f0b6409f501dbb75a061081c4bb18a99edd7
Ben Noordhuis <info@bnoordhuis.nl>
2014-06-03 12:26:13 +0200

    src: fix use of uninitialized signal handle
    With NODE_HEAPDUMP_OPTIONS=nosignal, the SIGCHLD signal handle wasn't
    initialized even though it's used by heapdump::WriteSnapshot().
    
    See discussion in #16.

--- src/platform-posix.cc
+++ src/platform-posix.cc
@@ -85,12 +85,12 @@ void PlatformInit(Isolate* isolate)
 {
   const char* options = getenv("NODE_HEAPDUMP_OPTIONS");
   if (options == NULL || strcmp(options, "nosignal") != 0) {
-    uv_signal_init(uv_default_loop(), &sigchld_handle);
     uv_signal_init(uv_default_loop(), &signal_handle);
     uv_signal_start(&signal_handle, OnSIGUSR2, SIGUSR2);
     uv_unref(reinterpret_cast<uv_handle_t*>(&signal_handle));
     signal_handle.data = isolate;
   }
+  uv_signal_init(uv_default_loop(), &sigchld_handle);
 }
 
 } // namespace heapdump
